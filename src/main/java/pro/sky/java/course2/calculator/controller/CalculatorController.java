0b566ce73a33f84250f3deb2908d162a9ac8506b
Viacheslav Privalov <privalov2306@yandex.ru>
2023-04-10 18:29:27 +0300

    Первый рабочий вариант без service

--- src/main/java/pro/sky/java/course2/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/java/course2/calculator/controller/CalculatorController.java
@@ -1,12 +1,36 @@
 package pro.sky.java.course2.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
     @GetMapping
-    public String hello() {
-        return "Hello, world";
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam int num1,
+                      @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1,
+                      @RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int num1,
+                      @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+    @GetMapping("/divide")
+    public String divide(@RequestParam int num1,
+                      @RequestParam int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
     }
 }
